fed9dbe4734a29b5468be4330caf740e4f997250
UID3180101323 <3180101323@zju.edu.cn>
2020-12-15 10:31:04 +0800

    app.json中新增用户注册功能，获取用户openid，avatarUrl写入数据库

--- app.js
+++ app.js
@@ -3,44 +3,60 @@ App({
   globalData: {
     userInfo: null,
     appId:"wx8d5a947dca8f7394",
-    secret:"bcd0ad6883cd9440b12605608cccb787"
+    secret:"bcd0ad6883cd9440b12605608cccb787",
+    openid:null,
+    getUserInfoReady:false
   },
   onLaunch: function () {
     // 展示本地存储能力
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
-
-    // 登录
-    wx.cloud.init()
-    wx.cloud.callFunction({
-      // 云函数名称
-      name: 'getProj',
-      // 传给云函数的参数
-      data: {
-      },
-      success: function(res) {
-        console.log(res.result) // 3
-      },
-      fail: console.error
-    })
     // 获取用户信息
     wx.getSetting({
       success: res => {
         if (res.authSetting['scope.userInfo']) {
           // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          var userBasicInfo;
+          var userOpenId;
           wx.getUserInfo({
             success: res => {
               // 可以将 res 发送给后台解码出 unionId
+              userBasicInfo=res.userInfo;
+              //console.log(userBasicInfo)
               this.globalData.userInfo = res.userInfo
-              console.log(res.userInfo)
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
+              wx.cloud.init()
+              wx.cloud.callFunction({
+                // 云函数名称
+                name: 'getProj',
+                // 传给云函数的参数
+                data: {},
+                success: function(res) {
+                  userOpenId=res.result.userInfo.openId;
+                  //console.log(userOpenId);
+                  wx.request({
+                    url: 'http://wychandsome12138.xyz:996/api/post/user_sign',
+                    method: "POST",
+                    header: {
+                      'content-type': 'application/json' 
+                    },
+                    data:{
+                      "id": userOpenId,
+                      "url":userBasicInfo.avatarUrl,
+                      "name":userBasicInfo.nickName
+                    },
+                    success: function(res){
+                      console.log(res)
+                      },
+                      fail: function(res){
+                        console.log("请求User login的request 失败！")
+                      }
+                    })
+                  },
+                  fail: console.error
+                })
+                  }
+                })
         }
       }
     })
@@ -50,8 +66,9 @@ App({
         let custom = wx.getMenuButtonBoundingClientRect();
         this.globalData.Custom = custom;  
         this.globalData.CustomBar = custom.bottom + custom.top - e.statusBarHeight;
+        
       }
-    })  
+    })          
 }
-
+  
 })
